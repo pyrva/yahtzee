2bed7cd9ef094a77d046e58ebf7b7b1abde524eb
Brian Cohan <briancohan@gmail.com>
2024-01-15 11:59:15 -0500

    start

--- test_yahtzee.py
+++ test_yahtzee.py
@@ -12,170 +12,71 @@ def test__roll__returns_dice_with_correct_values():
         assert die in range(1, 7)
 
 
-@pytest.mark.parametrize(
-    "dice, keep, valid",
-    [
-        ([1, 2, 3, 4, 5], [1, 2, 3], True),
-        ([1, 2, 2, 4, 5], [1, 2, 4, 5], True),
-        ([1, 2, 3, 4, 5], [1, 1, 3], False),
-    ],
-)
+@pytest.mark.parametrize("dice, keep, valid", [])
 def test__keep__is_valid(dice, keep, valid):
     assert is_valid_to_keep(dice, keep) is valid
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([2, 2, 2, 2, 2], 0),
-        ([1, 1, 1, 1, 1], 5),
-        ([1, 1, 1, 1, 2], 4),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__ones(dice, expected):
     assert score_ones(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 1, 1, 1, 1], 0),
-        ([2, 2, 2, 2, 2], 10),
-        ([2, 2, 2, 2, 1], 8),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__twos(dice, expected):
     assert score_twos(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 1, 1, 1, 1], 0),
-        ([3, 3, 3, 3, 3], 15),
-        ([3, 3, 3, 3, 1], 12),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__threes(dice, expected):
     assert score_threes(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 1, 1, 1, 1], 0),
-        ([4, 4, 4, 4, 4], 20),
-        ([4, 4, 4, 4, 1], 16),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__fours(dice, expected):
     assert score_fours(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 1, 1, 1, 1], 0),
-        ([5, 5, 5, 5, 5], 25),
-        ([5, 5, 5, 5, 1], 20),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__fives(dice, expected):
     assert score_fives(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 1, 1, 1, 1], 0),
-        ([6, 6, 6, 6, 6], 30),
-        ([6, 6, 6, 6, 1], 24),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__sixes(dice, expected):
     assert score_sixes(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 2, 3, 4, 5], 0),
-        ([1, 1, 1, 1, 1], 5),
-        ([2, 2, 2, 4, 2], 12),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__3_of_a_kind(dice, expected):
     assert score_3_of_a_kind(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 2, 3, 4, 5], 0),
-        ([1, 1, 1, 1, 1], 5),
-        ([2, 2, 2, 1, 2], 9),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__4_of_a_kind(dice, expected):
     assert score_4_of_a_kind(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 2, 3, 3, 3], 0),
-        ([1, 2, 3, 4, 4], 30),
-        ([2, 3, 4, 5, 5], 30),
-        ([3, 4, 5, 6, 6], 30),
-        ([1, 2, 3, 4, 5], 30),
-        ([2, 3, 4, 5, 6], 30),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__small_straight(dice, expected):
     assert score_small_straight(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 2, 3, 4, 4], 0),
-        ([1, 2, 3, 4, 5], 40),
-        ([2, 3, 4, 5, 6], 40),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__large_straight(dice, expected):
     assert score_large_straight(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 2, 3, 4, 5], 0),
-        ([1, 1, 1, 1, 1], 25),
-        ([1, 1, 1, 2, 2], 25),
-        ([1, 1, 2, 2, 2], 25),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__full_house(dice, expected):
     assert score_full_house(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 2, 3, 5, 6], 17),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__chance(dice, expected):
     assert score_chance(dice) == expected
 
 
-@pytest.mark.parametrize(
-    "dice, expected",
-    [
-        ([1, 1, 1, 1, 1], 50),
-        ([1, 1, 1, 1, 2], 0),
-    ],
-)
+@pytest.mark.parametrize("dice, expected", [])
 def test__score__yahtzee(dice, expected):
     assert score_yahtzee(dice) == expected
--- yahtzee.py
+++ yahtzee.py
@@ -1,55 +1,72 @@
-from random import randint
-from collections import Counter
-from functools import partial
-
-
 ########################################
 # Score categories
 ########################################
-def score_value(dice: list[int], value: int) -> int:
-    """Score sum of all dice of a given value.
+def score_ones(dice: list[int]) -> int:
+    """Score sum of all dice of a given value 1.
 
     Only the dice of the value are counted. The rest are ignored.
     """
-    return sum([d for d in dice if d == value])
+    pass
 
 
-score_ones = partial(score_value, value=1)
-score_twos = partial(score_value, value=2)
-score_threes = partial(score_value, value=3)
-score_fours = partial(score_value, value=4)
-score_fives = partial(score_value, value=5)
-score_sixes = partial(score_value, value=6)
+def score_twos(dice: list[int]) -> int:
+    """Score sum of all dice of a given value 2.
+
+    Only the dice of the value are counted. The rest are ignored.
+    """
+    pass
 
 
-def score_n_of_kind(dice: list, n: int) -> int:
-    """Score sum of all dice least n die with a given value."""
-    counts = Counter(dice)
-    if max(counts.values()) >= n:
-        return sum(dice)
-    return 0
+def score_threes(dice: list[int]) -> int:
+    """Score sum of all dice of a given value 3.
+
+    Only the dice of the value are counted. The rest are ignored.
+    """
+    pass
 
 
-score_3_of_a_kind = partial(score_n_of_kind, n=3)
-score_4_of_a_kind = partial(score_n_of_kind, n=4)
+def score_fours(dice: list[int]) -> int:
+    """Score sum of all dice of a given value 4.
+
+    Only the dice of the value are counted. The rest are ignored.
+    """
+    pass
+
+
+def score_fives(dice: list[int]) -> int:
+    """Score sum of all dice of a given value 5.
+
+    Only the dice of the value are counted. The rest are ignored.
+    """
+    pass
+
+
+def score_sixes(dice: list[int]) -> int:
+    """Score sum of all dice of a given value 6.
+
+    Only the dice of the value are counted. The rest are ignored.
+    """
+    pass
+
+
+def score_3_of_a_kind(dice: list) -> int:
+    """Score sum of all dice if at least 3 die with a given value."""
+    pass
+
+
+def score_4_of_a_kind(dice: list) -> int:
+    """Score sum of all dice if at least 4 die with a given value."""
+    pass
 
 
 def score_small_straight(dice: list[int]) -> int:
     """Score 30 if there is a straight of 4 dice."""
-    possible_straights = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
-    for straight in possible_straights:
-        if all(d in dice for d in straight):
-            return 30
-    return 0
+    pass
 
 
 def score_large_straight(dice: list[int]) -> int:
     """Score 40 if there is a straight of 5 dice."""
-    possible_straights = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6]]
-    for straight in possible_straights:
-        if all(d in dice for d in straight):
-            return 40
-    return 0
+    pass
 
 
 def score_full_house(dice: list[int]) -> int:
@@ -57,23 +74,17 @@ def score_full_house(dice: list[int]) -> int:
 
     Allow for 5 of a kind to count as a pair and a three of a kind.
     """
-    counts = Counter(dice)
-    print(counts)
-    if len(counts) == 2 and 2 in counts.values():
-        return 25
-    if len(counts) == 1:
-        return 25
-    return 0
+    pass
 
 
 def score_chance(dice: list[int]) -> int:
     """Score sum of all dice."""
-    return sum(dice)
+    pass
 
 
 def score_yahtzee(dice: list[int]) -> int:
     """Score 50 if all dice are the same."""
-    return 50 if len(set(dice)) == 1 else 0
+    pass
 
 
 ########################################
@@ -81,54 +92,25 @@ def score_yahtzee(dice: list[int]) -> int:
 ########################################
 def roll(n: int) -> list[int]:
     """Rolls n dice and returns the result in a list."""
-    return [randint(1, 6) for _ in range(n)]
+    pass
 
 
 def is_valid_to_keep(dice: list[int], keep: list[int]) -> bool:
     """Checks if the dice to keep are valid."""
-    _dice = dice.copy()
-    try:
-        for d in keep:
-            _dice.remove(d)
-    except ValueError:
-        return False
-
-    return True
+    pass
 
 
 def prompt_dice_to_keep(dice: list[int]) -> list[int]:
     """Prompts the user to keep dice and returns the result in a list."""
-    print("Your Dice:", ", ".join([str(d) for d in sorted(dice)]))
-    while True:
-        keep = input("Dice to keep (values): ")
-        keep = [int(d) for d in keep if d.isdigit()]
-        if is_valid_to_keep(dice, keep):
-            return keep
-        else:
-            print("Invalid dice to keep!")
+    pass
 
 
 def prompt_category_to_score(scores: dict) -> str:
-    selection = 0
-    valid = []
-    print("Score dice as:")
-    for i, (k, v) in enumerate(scores.items(), 1):
-        if not v["used"]:
-            valid.append(i)
-            print(f"> {i}. {k}")
-    while selection not in valid:
-        selection = int(input("Selection: "))
-
-    return list(scores.keys())[selection - 1]
+    pass
 
 
 def show_scores(scores: dict):
-    print("Scoreboard:")
-    chars = max([len(k) for k in scores.keys()]) + 2
-    for k, v in scores.items():
-        print(f"{k:.<{chars}} {v['score']}")
-
-    print(f"{'TOTAL':_<{chars}} {sum([v['score'] for v in scores.values()])}")
+    pass
 
 
 def main():
@@ -147,21 +129,6 @@ def main():
         "Chance": {"score": 0, "used": False, "func": score_chance},
         "Yahtzee": {"score": 0, "used": False, "func": score_yahtzee},
     }
-
-    while any(v["used"] == False for v in scores.values()):
-        dice = roll(5)
-        for _ in range(2):
-            dice = prompt_dice_to_keep(dice)
-            if len(dice) == 5:
-                break
-            else:
-                dice += roll(5 - len(dice))
-
-        print("Final Dice:", ", ".join([str(d) for d in sorted(dice)]))
-
-        category = prompt_category_to_score(scores)
-        scores[category]["score"] = scores[category]["func"](dice)
-        show_scores(scores)
 
 
 if __name__ == "__main__":
